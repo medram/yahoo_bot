806591906e038fbe2836e68992a851f05fb38908
medram <mramouchy@gmail.com>
2020-03-12 18:50:54 +0100

    nothing important

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Firefox()
-driver.maximize_window()
+# driver.maximize_window()
 driver.get("https://login.yahoo.com/?.src=ym&.lang=en-BE&.intl=be&.done=https%3A%2F%2Fmail.yahoo.com%2Fd")
 # assert "Python" in driver.title
 print(driver.title)
@@ -30,10 +30,33 @@ password.send_keys(Keys.ENTER)
 driver.implicitly_wait(2)
 # driver.switch_to.frame(0)
 
-spam_link = driver.find_element(By.CLASS_NAME, 'rtlI_dz_sSg ')
-action = ActionChains(driver)
-action.move_to_element(spam_link)
-action.click(spam_link)
-action.perform()
+spam_link = driver.find_elements_by_css_selector('a.W_6D6F')
+# Spam section
+spam_link[6].send_keys(Keys.RETURN)
+
+driver.implicitly_wait(2)
+
+
+
+
+# action = ActionChains(driver)
+# action.move_to_element(spam_link)
+# action.click(spam_link)
+# action.perform()
+
+# emails = driver.find_elements(By.CLASS_NAME, 'c27KHO0_n')
+driver.implicitly_wait(2)
+print(driver.window_handles)
+messages = driver.find_elements_by_css_selector('button.c27KHO0_n')
+# messages = driver.find_elements_by_css_selector('button[data-test-id="icon-btn-checkbox"] span.D_F')
+
+# for message in messages:
+# 	message.click()
+# print(messages)
+# https://www.programcreek.com/python/example/100026/selenium.webdriver.FirefoxProfile
+driver.execute_script("""
+	console.log(arguments)
+	arguments[0].click()
+""", messages)
 
 # driver.close()
